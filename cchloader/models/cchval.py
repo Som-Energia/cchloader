6960692df24c5d33e019a0e52aec1cd675202a98
Agustí Fita i Borrell <afita@gisce.net>
2016-04-25 10:25:21 +0200

    FIX season as interger value

--- cchloader/models/cchval.py
+++ cchloader/models/cchval.py
@@ -6,6 +6,6 @@ from marshmallow.validate import OneOf
 class CchValSchema(Schema):
     name = fields.String(position=0, required=True)
     datetime = fields.DateTime(position=1, format='%Y/%m/%d %H:%M')
-    season = fields.Str(position=2, validate=OneOf([0, 1]))
+    season = fields.Integer(position=2, validate=OneOf([0, 1]))
     ai = fields.Integer(position=3)
     ao = fields.Integer(position=4, allow_none=True)
