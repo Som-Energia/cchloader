89d8c0e35029ffcc22fc467e4a3687956a80a5ee
Arua Ibrahim <aibrahim@abenergia.es>
2017-03-09 10:26:55 +0100

    Add models/parsers/adapters for p1 p1d f1
    Including a parser for p1d that dont follow the filename rules

---
+++ cchloader/adapters/p1.py
@@ -0,0 +1,36 @@
+from cchloader.adapters import CchAdapter
+from cchloader.models.p1 import P1Schema
+from marshmallow import Schema, fields, pre_load
+
+class P1BaseAdapter(Schema):
+    """ P1 Adapter
+    """
+
+    @pre_load
+    def fix_numbers(self, data):
+        for attr, field in self.fields.iteritems():
+            if isinstance(field, fields.Integer):
+                if not data.get(attr):
+                    data[attr] = None
+        return data
+
+    @pre_load
+    def fix_season(self, data):
+        valid_values = [0, 1]
+        season = data.get('season')
+        if season and season.isdigit() and season in map(str, valid_values):
+            data['season'] = int(season)
+        else:
+            data['season'] = None
+
+    @pre_load
+    def fix_source(self, data):
+        valid_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 22]
+        source = data.get('source')
+        if source and source.isdigit() and int(source) in valid_values:
+            data['source'] = int(source)
+        else:
+            data['source'] = None
+
+class P1Adapter(P1BaseAdapter, CchAdapter, P1Schema):
+    pass
---
+++ cchloader/parsers/p1.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+from cchloader import logger
+from cchloader.utils import build_dict
+from cchloader.adapters.p1 import P1Adapter
+from cchloader.models.p1 import P1Schema
+from cchloader.parsers.parser import Parser, register
+
+
+class P1(Parser):
+
+    pattern = '^P1_(\d{4})_(\d{4})(\d{2})(\d{2})_(\d{4})(\d{2})(\d{2}).(\d)'
+    encoding = "iso-8859-15"
+    delimiter = ';'
+
+    def __init__(self, strict=False):
+        self.adapter = P1Adapter(strict=strict)
+        self.schema = P1Schema(strict=strict)
+        self.fields = []
+        self.headers = []
+        for f in sorted(self.schema.fields,
+                key=lambda f: self.schema.fields[f].metadata['position']):
+            field = self.schema.fields[f]
+            self.fields.append((f, field.metadata))
+            self.headers.append(f)
+
+    def parse_line(self, line):
+        slinia = tuple(unicode(line.decode(self.encoding)).split(self.delimiter))
+        slinia = map(lambda s: s.strip(), slinia)
+        parsed = {'p1': {}, 'orig': line}
+        data = build_dict(self.headers, slinia)
+        result, errors = self.adapter.load(data)
+        if errors:
+            logger.error(errors)
+        parsed['p1'] = result
+        return parsed, errors
+
+
+register(P1)
