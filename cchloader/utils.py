a0033cd6483bb8ad6b63c6fde003b91caccbd8c9
tinogis <afita@gisce.net>
2017-04-18 16:30:26 +0200

    IMP Only when 22 chars

--- cchloader/utils.py
+++ cchloader/utils.py
@@ -21,7 +21,7 @@ def get_curve_cups(cups_name):
     Gets cups to search curve by cups. Some DFO's change termination in
     x5D files (i,e, FENOSA puts 1F instead of 1P)
     """
-    if cups_name[-2:] == '1P':
+    if len(cups_name) == 22 and cups_name[-2:] == '1P':
         cups = cups_name[:20]
         return cups + '1F'
     else:
