91ea9662b454485b63739e350efd09848bad66cb
MiquelIR <misernroca@gisce.net>
2017-07-10 12:19:59 +0200

    Test-Add test to get exception on wrong filename

--- spec/test_parsers.py
+++ spec/test_parsers.py
@@ -1,7 +1,9 @@
-from expects import expect, equal
+from expects import expect, equal, raise_error
 from cchloader.parsers.parser import *
 from cchloader.parsers.f1 import F1
 from cchloader.parsers.p1 import P1
+from cchloader.exceptions import ParserNotFoundException
+
 
 with description('Testing of parsers'):
 
@@ -17,6 +19,7 @@ with description('Testing of parsers'):
             'P1_0022_0706_20170507_20170706.6',  # Fenosa
             'P1_0031_20170311.1.ZIP',  # Endesa
         ]
+        self.wrong_filename = 'P1_20170507_20170706.6'
 
     with it('test to get F1 parser'):
         for filename in self.f1_filenames:
@@ -25,3 +28,10 @@ with description('Testing of parsers'):
     with it('test to get P1 parser'):
         for filename in self.p1_filenames:
             expect(get_parser(filename)).to(equal(P1))
+
+    with it('test error to get exception'):
+        def test_raise_error():
+            get_parser(self.wrong_filename)
+
+        expect(test_raise_error).to(raise_error(
+            ParserNotFoundException))
