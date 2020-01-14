4053b79a603265202fa5a406d3f6ef0a1db9957f
david <dmunoz@gisce.net>
2020-01-14 12:04:51 +0100

    Added test to check if P2 files have a right parser

--- spec/test_parsers_spec.py
+++ spec/test_parsers_spec.py
@@ -2,6 +2,7 @@ from expects import expect, equal, raise_error
 from cchloader.parsers.parser import *
 from cchloader.parsers.f1 import F1
 from cchloader.parsers.p1 import P1
+from cchloader.parsers.p2 import P2
 from cchloader.exceptions import ParserNotFoundException
 
 
@@ -19,6 +20,11 @@ with description('Testing of parsers'):
             'P1_0022_0706_20170507_20170706.6',  # Fenosa
             'P1_0031_20170311.1.ZIP',  # Endesa
         ]
+        self.p2_filenames = [
+            'P2_0022_20170507_20170706.6',  # Documented
+            'P2_0022_0706_20170507_20170706.6',  # Fenosa
+            'P2_0031_20170311.1.ZIP',  # Endesa
+        ]
         self.wrong_filename = 'P1_20170507_20170706.6'
 
     with it('test to get F1 parser'):
@@ -28,6 +34,10 @@ with description('Testing of parsers'):
     with it('test to get P1 parser'):
         for filename in self.p1_filenames:
             expect(get_parser(filename)).to(equal(P1))
+
+    with it('test to get P2 parser'):
+        for filename in self.p2_filenames:
+            expect(get_parser(filename)).to(equal(P2))
 
     with it('test error to get exception'):
         def test_raise_error():
