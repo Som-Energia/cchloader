46084968574a9be99961646f9f081c52af3dd7e0
Oriol Piera <oriol.piera@somenergia.coop>
2023-11-03 10:17:58 +0100

    Bump to v0.4.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ readme = open("README.md").read()
 
 setup(
     name='cchloader',
-    version='0.4.4',
+    version='0.4.5',
     packages=find_packages(),
     url='https://github.com/Som-Energia/cchloader',
     license='GPLv3',
