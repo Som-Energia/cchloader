2b9ad43f34150cdec5259d19fdedc0a8a2e909d3
davidmunoznovoa <dmunoz@gisce.net>
2024-03-13 13:32:52 +0100

    Add text for corbagen

--- cchloader/models/corbagen.py
+++ cchloader/models/corbagen.py
@@ -7,7 +7,7 @@ from marshmallow.validate import OneOf
 
 class CorbaGenSchema(Schema):
     name = fields.String(position=0, required=True)
-    datetime = fields.DateTime(position=1, format='%Y-%m-%d %H:%M:%S', required=True)
+    datetime = fields.DateTime(position=1, format='%Y-%m-%d %H:%M', required=True)
     season = fields.Integer(position=2, validate=OneOf([0, 1]), required=True)
     generacio = fields.Integer(position=3, required=True)
     autocons = fields.Integer(position=4, required=True)
