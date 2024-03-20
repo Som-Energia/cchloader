db11be055601dabd003b49e410967109b576213b
davidmunoznovoa <dmunoz@gisce.net>
2024-03-20 17:12:44 +0100

    Bump to v0.4.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES_DATA = {'cchloader': ['data/*']}
 
 setup(
     name='cchloader',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
     url='https://github.com/gisce/cchloader',
     license='GPLv3',
