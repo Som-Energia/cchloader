b56ecfbed0dab1e7d2f6d971759f6261130e94c1
PSala <polsalalidon@gmail.com>
2024-04-09 17:35:19 +0200

    Bump to v3.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ PACKAGES_DATA = {'cchloader': ['data/*']}
 
 setup(
     name='cchloader',
-    version='3.0.0',
+    version='3.0.1',
     packages=find_packages(),
     url='https://github.com/gisce/cchloader',
     license='GPLv3',
